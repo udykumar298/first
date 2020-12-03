d596f4413137b24853eea524f40b478d53aedf68
udaykumar298 <v.udaykumar222@gmail.com>
2020-12-03 23:12:20 +0530

    initial commit

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+
+class Excp extends Throwable{
+
+    public void m1(){
+        try {
+            throw new IOException("array not there");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally{
+            System.out.println("Bye");
+        }
+
+
+    }
+
+}
 public class demo {
+    public static  void main(String [] args){
+        Excp m=new Excp();
+        m.m1();
+    }
 }
