cf5eb642f8055d57fc94cd881dd109765becb2cf
udaykumar298 <v.udaykumar222@gmail.com>
2020-12-03 23:45:49 +0530

    second commit

---
+++ src/main/java/test.java
@@ -0,0 +1,21 @@
+class Student{
+    //private data member
+    private String name;
+    //getter method for name
+    public String getName(){
+        return name;
+    }
+    //setter method for name
+    public void setName(String name){
+        this.name=name;
+    }
+}
+public class test {
+    public static void main(String [] args){
+        Student s=new Student();
+//setting value in the name member
+        s.setName("vijay");
+//getting value of the name member
+        System.out.println(s.getName());
+    }
+}
